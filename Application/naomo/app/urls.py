d4654e965de03924866c01a111c6bea5e495d98a
Kanta Kurazumi <krzmknt@gmail.com>
2021-03-28 15:22:00 +0000

    Add /app/

--- Application/naomo/app/urls.py
+++ Application/naomo/app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from app.views.login import view as login_view
+from app.views.index import view as index_view
 
 urlpatterns = [
-    path("", login_view.top, name="login_top")
+    path('login', login_view.top, name="login_top"),
+    path('', index_view.top, name="index_top"),
 ]
